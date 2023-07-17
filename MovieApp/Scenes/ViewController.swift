1bc4e3b6e8c65afcd86df585d88a935ed0069abc
anadzebniauri <anadzebniauri13@gmail.com>
2023-07-17 13:58:26 +0400

    Initial commit

---
+++ MovieApp/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Ana Dzebniauri on 17.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
